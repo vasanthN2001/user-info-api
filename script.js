bea48c5d6bc955a6e1235888decb1cf102402f41
nmkyuppie <nmkyuppie@gmail.com>
2023-05-24 00:02:29 +0530

    CSS changes

--- script.js
+++ script.js
@@ -9,25 +9,22 @@ const fetchUserDetails = () => {
     const lastName = userData.last_name;
     const city = userData.address.city;
     const email = userData.email;
-    const gender = userData.gender;
     const number = userData.phone_number;
-    const country = userData.country;
+    const country = userData.address.country;
     const dateOfBirth = userData.date_of_birth;
     const userName = userData.username;
     const id = userData.id;
 
     //About
-    document.getElementById("firstName").innerHTML = "First-Name  :" + firstName;
-    document.getElementById("lastName").innerHTML = "Second-Name  :" + lastName;
-    document.getElementById("email").innerHTML = "Email  :" + email;
+    document.getElementById("name").innerHTML =  firstName + " " + lastName ;
+    document.getElementById("email").innerHTML ="📧 " + email;
     //Details
-    document.getElementById("city").innerHTML = "City  " + city;
-    document.getElementById("dateOfBirth").innerHTML = "date_of_birth  " + dateOfBirth;
-    document.getElementById("gender").innerHTML = "Gender  " + gender;
-    document.getElementById("number").innerHTML = "Phone-Number  " + number;
-    document.getElementById("country").innerHTML = "Country  " + country;
-    document.getElementById("userName").innerHTML = "Username  " + userName;
-    document.getElementById("id").innerHTML = "Id  " + id;
+    document.getElementById("city").innerHTML = "🌍 " + city;
+    document.getElementById("dateOfBirth").innerHTML = "🎂 " + dateOfBirth;
+    document.getElementById("number").innerHTML = "📞 " + number;
+    document.getElementById("country").innerHTML = "🌍 " + country;
+    document.getElementById("userName").innerHTML = "👨 " + userName;
+    document.getElementById("id").innerHTML = "🔐 " + id;
   };
   xhttp.open("GET", "https://random-data-api.com/api/v2/users", true);
   xhttp.send();
